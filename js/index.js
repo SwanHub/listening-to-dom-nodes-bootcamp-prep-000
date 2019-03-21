c7a52fa88107872d0dc48a1ceb22d4412a2a8be4
Jackson Prince <jdprince555@gmail.com>
2019-03-21 02:45:32 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
+const divs = document.querySelectorAll('div');
+
 function bubble(e) {
     e.stopPropagation();
     console.log(this.firstChild.nodeValue.trim() + ' bubbled');}
 
+for (let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', bubble);
+}
